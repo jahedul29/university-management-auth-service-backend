648c05051706362be97e560a87de8cb3e080a24b
JAHED <macbookpro@MacBooks-MacBook-Pro.local>
2023-06-02 22:13:43 +0600

    UM-7 User interface and model done

---
+++ src/app/modules/users/users.interface.ts
@@ -0,0 +1,11 @@
+import { Model } from 'mongoose'
+
+export type IUser = {
+  id: string
+  role: string
+  password: string
+}
+
+export type IUserMethods = object
+
+export type UserModel = Model<IUser, object, IUserMethods>
